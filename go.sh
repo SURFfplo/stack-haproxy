80574cc85f6830c9a6fab2a5db635fcfabef6586
Herman <herman@dompseler.nl>
2019-06-11 23:44:55 +0200

    set config only the first time & use variables

--- go.sh
+++ go.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+export STACK_NETWORK=platform-net
+export STACK_SERVICE=haproxy
+export STACK_VERSION=0.1
+
 # prepare
 ./prepare.sh
 
 # go
-docker stack deploy -c docker-compose.yml haproxy
+docker stack deploy -c docker-compose.yml $STACK_SERVICE
--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# todo
-# - two variables: network + containername
+DIR_CONF=/mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE/$STACK_VERSION/conf
 
-# create nfs mount
-mkdir -p /mnt/nfs/nfsdlo/platform-net/haproxy-0.1/
-
-# write data
-cp -a config/haproxy.cfg /mnt/nfs/nfsdlo/platform-net/haproxy-0.1/
+if [ ! -d "$DIR_CONF" ]; then
+	# create nfs mount
+	mkdir -p $DIR_CONF
+	# write data
+	cp -a config/* $DIR_CONF
+fi
